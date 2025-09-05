28be80d418d544390b49c72f2d91542fa6ddf01b
Elton Lino <eltonblino@gmail.com>
2025-09-05 06:37:22 -0300

    laço for

--- aula13/repeticao.js
+++ aula13/repeticao.js
@@ -1,13 +1,20 @@
-// Estrutura de repetição com teste lógico no início
+// Estrutura de repetição com teste lógico no início - while
 var c = 1;
 while (c <= 5) {
     console.log(`Passo ${c}`);
     c++;
 }
+
 console.log('------------------');
-// Estrutura de repetição com teste lógico no final
+// Estrutura de repetição com teste lógico no final - do...while
 var c = 1;
 do {
     console.log(`Passo ${c}`);
     c++;
 } while (c <= 5);
+
+console.log('------------------');
+// Estrutura de repetição com variável de controle - for
+for (var c = 1; c <= 5; c++) {
+    console.log(`Passo ${c}`);
+}
