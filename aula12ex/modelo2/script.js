52d5b482b034011cd88d90f29f95303612860550
Elton Lino <eltonblino@gmail.com>
2025-09-05 05:47:52 -0300

    Completando exercicio

--- aula12ex/modelo2/script.js
+++ aula12ex/modelo2/script.js
@@ -8,5 +8,45 @@ function verificar(){
     } else {
         var fsex = document.getElementsByName('radsex');
         var idade = ano - Number(fano.value);
-        res.innerHTML = `Idade calculada: ${idade} anos.`
-}}
+        var genero = '';
+        var img = document.createElement('img');
+        img.setAttribute('id', 'foto');
+        if (fsex[0].checked){
+            genero = 'Homem';
+            if(idade >=0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'img/bebe-m.png');
+            } else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'img/jovem-m.png');
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'img/adulto-m.png');
+            } else {
+                //Idoso
+                img.setAttribute('src', 'img/idoso-m.png');
+            }
+        }else if (fsex[1].checked){
+            genero = 'Mulher';
+            if(idade >=0 && idade < 10){
+                //Criança
+                img.setAttribute('src', 'img/bebe-f.png');
+            } else if (idade < 21){
+                //Jovem
+                img.setAttribute('src', 'img/jovem-f.png');
+            } else if (idade < 50){
+                //Adulto
+                img.setAttribute('src', 'img/adulto-f.png');
+            } else {
+                //Idoso
+                img.setAttribute('src', 'img/idoso-f.png');
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+        img.setAttribute('id', 'foto');
+        img.style.display = 'block';
+        img.style.margin = '10px auto';
+    }
+}
