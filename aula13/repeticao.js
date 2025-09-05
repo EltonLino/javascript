f2c236a833e2e4c6e42f2135590b0ed8a3f62c3e
Elton Lino <eltonblino@gmail.com>
2025-09-05 06:16:36 -0300

    Estruturas de repetição

---
+++ aula13/repeticao.js
@@ -0,0 +1,13 @@
+// Estrutura de repetição com teste lógico no início
+var c = 1;
+while (c <= 5) {
+    console.log(`Passo ${c}`);
+    c++;
+}
+console.log('------------------');
+// Estrutura de repetição com teste lógico no final
+var c = 1;
+do {
+    console.log(`Passo ${c}`);
+    c++;
+} while (c <= 5);
