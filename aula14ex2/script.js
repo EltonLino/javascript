86f5e408af7884f7ea88e0fb972976e2b75b98f1
Elton Lino <eltonblino@gmail.com>
2025-09-10 06:18:56 -0300

    exercicio tabuada

--- aula14ex2/script.js
+++ aula14ex2/script.js
@@ -3,8 +3,15 @@ function tabuada() {
     let tab = document.getElementById('seltab')
     
     if (num.value.length == 0) {
-         let n = Number(num.value)
-    } else {
         window.alert('Por favor, digite um número!')
+    } else {
+        let n = Number(num.value)
+        tab.innerHTML = ''
+        for (let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n* c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
     }
+}
 }
